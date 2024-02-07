13f18897797ff2fb53a073b7eaf4ad564e843716
syipmong <syipmong5@gmail.com>
2024-02-07 14:46:28 +0100

    regularize the api usage

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,13 @@ import 'firebase/compat/storage';
 
 // require('dotenv').config();
 const firebaseConfig = {
-   apiKey: "AIzaSyAI6-TkzIVAfxjKq-Qq6Z9oEibc1hFsLBo",
-  authDomain: "dr-amina.firebaseapp.com",
-  projectId: "dr-amina",
-  storageBucket: "dr-amina.appspot.com",
-  messagingSenderId: "988275447318",
-  appId: "1:988275447318:web:934cf298f6cf4bf2bd8737",
-  measurementId: "G-073K35HBX4"
+    apiKey: "AIzaSyAI6-TkzIVAfxjKq-Qq6Z9oEibc1hFsLBo",
+    authDomain: "dr-amina.firebaseapp.com",
+    projectId: "dr-amina",
+    storageBucket: "dr-amina.appspot.com",
+    messagingSenderId: "988275447318",
+    appId: "1:988275447318:web:934cf298f6cf4bf2bd8737",
+    measurementId: "G-073K35HBX4"
 };
 
 firebase.initializeApp(firebaseConfig);
---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+
+
+module.exports = {
+  resolve: {
+    fallback: {
+      "path": require.resolve("path-browserify"),
+      "os": require.resolve("os-browserify/browser"),
+      "crypto": require.resolve("crypto-browserify")
+    }
+  }
+};
